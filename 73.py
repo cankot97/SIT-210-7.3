676bb9f7a89618b4b965c50b136c656565c8fada
cankot97 <63225267+cankot97@users.noreply.github.com>
2020-05-19 17:53:26 +1000

    Add files via upload

---
+++ 73.py
@@ -0,0 +1,62 @@
+import RPi.GPIO as GPIO
+import time
+ 
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BOARD)
+ 
+#set GPIO Pins
+GPIO_TRIGGER = 10
+GPIO_ECHO = 8
+ 
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+GPIO.setup(12, GPIO.OUT)  # Set GPIO pin 12 to output mode.
+pwm = GPIO.PWM(12, 100)   # Initialize PWM on pwmPin 100Hz frequency
+dc=0                               
+pwm.start(dc)  
+ 
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+ 
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = distance()
+            if (dist > 50):
+                pwm.ChangeDutyCycle(0)
+            elif (dist <= 0):
+                pwm.ChangeDutyCycle(100)
+            else:
+                pwm.ChangeDutyCycle(100 - 2*dist)
+            print ("Measured Distance = %.1f cm" % dist)
+            time.sleep(0.1)
+ 
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
